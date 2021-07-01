9968441f5ec8f09f7e35284d90e7748f009bdc9c
mjsigg <mjsigg@gmail.com>
2021-06-30 23:04:47 -0500

    all the changes

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -1,10 +1,16 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const password = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const password = req.query.password || req.body.password;
+
+    let your_response;
+
+    if (password == "letmein"){
+            your_response = 'Access granted.'
+    } else{
+            your_response = "Access denied."
+    }
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
