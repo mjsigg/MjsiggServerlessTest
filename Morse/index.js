e94cc29a780eb24cb15a1e82f14e09f301e2accd
mjsigg <mjsigg@gmail.com>
2021-07-06 14:00:34 -0500

    Added morse text editor

---
+++ Morse/index.js
@@ -0,0 +1,18 @@
+const morse = require("morse-code-converter");
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const plaintext = (req.query.plaintext || (req.body && req.body.name));
+  
+    const morse = require("morse-code-converter");
+ 
+    const code = morse.textToMorse('Hey how are you?'); // .... . -.--   .... --- .--   .- .-. .   -.-- --- ..- ..-..
+    const text = morse.morseToText(code); // HEY HOW ARE YOU?
+
+    context.res = {
+        body: code
+    };
+
+}
