2c357d790aaab4335e193aec81c05ea46b0bc553
mjsigg <mjsigg@gmail.com>
2021-07-06 12:39:13 -0500

    Added a name generator and 2 cat images.

--- twocatz/index.js
+++ twocatz/index.js
@@ -1,9 +1,19 @@
 const fetch = require('node-fetch')
 
+function gimmename() {
+
+    var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+    var random_value = Math.floor(names.length * Math.random())
+    var resultname = names[random_value]
+
+    return resultname
+}
+
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let THE_ENDPOINT = "https://cataas.com/cat/cute/says/serverless"
+    let THE_ENDPOINT = "https://cataas.com/cat/cute/says/Bitcamp"
 
     let resp = await fetch(THE_ENDPOINT, {
         method: 'GET'
@@ -13,10 +23,25 @@ module.exports = async function (context, req) {
 
     let base64data = Buffer.from(data).toString('base64')
 
+    let name1 = gimmename()
+    let name2 = gimmename()
+    let cat1 = get_cat()
+    let cat2 = get_cat()
+    
 
+function get_cat() {
 
+        return resp 
+     }
+ 
     context.res = {
         
-        body: base64data
+        body: {
+            cat1: base64data,
+            cat2: base64data,
+            names: [name1, name2]
+            
+    }
+        
     };
 }
