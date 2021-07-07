ae5ddc36ac3576d0f225b045722ba752d70574d2
mjsigg <mjsigg@gmail.com>
2021-07-06 21:02:57 -0500

    yes?

--- morse/index.js
+++ morse/index.js
@@ -3,10 +3,9 @@ const morse = require("morse-code-converter");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const plaintext = (req.query.plaintext)
+    const plaintext = req.query.plaintext;
  
     const code = morse.textToMorse(plaintext); 
-    const text = morse.morseToText(code); 
     
 
     context.res = {
