6892f963f64f68de360de1057d26c7d4d300a224
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-07-07 08:00:35 -0400

    Create index.js

---
+++ morse/index.js
@@ -0,0 +1,13 @@
+const morse = require("morse-code-converter");
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    
+    const plaintext = req.query.plaintext;
+
+    const code = morse.textToMorse(plaintext); 
+
+    context.res = {
+        body: code
+    };
+}
