78ac92d8eedc8003f8587657bafa14fe13cb797d
mjsigg <mjsigg@gmail.com>
2021-07-06 21:45:10 -0500

    plz

--- morse/index.js
+++ morse/index.js
@@ -2,10 +2,6 @@ const morse = require("morse-code-converter");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const plaintext = req.query.plaintext;
- 
-    const code = morse.textToMorse(plaintext); 
     
 function message (){
 
@@ -13,7 +9,7 @@ function message (){
     const plaintext = req.query.plaintext;
 
     return message('Yyyooooo')
-}
+};
 
     context.res = {
         body: code
