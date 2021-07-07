14004f2dee2d6183c27c0731c86a37cd918f140e
mjsigg <mjsigg@gmail.com>
2021-07-06 22:14:19 -0500

    morse code

--- morse/index.js
+++ morse/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
 const code = morse.textToMorse('Hey how are you?'); // .... . -.--   .... --- .--   .- .-. .   -.-- --- ..- ..-..
-const text = morse.morseToText(code); // HEY HOW ARE YOU?
+
 
     context.res = {
         body: code
