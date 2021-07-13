a97167ac8a03fa53a5186c611bf43373fcfadc50
mjsigg <mjsigg@gmail.com>
2021-07-13 12:06:11 -0500

    added async function <analyzeImage> successfully

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,19 +1,37 @@
 var multipart = require('parse-multipart');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
-
-// here's your boundary:
     var boundary = multipart.getBoundary(req.headers['content-type']);
-  
     var body = req.body
-
-    var parts = multipart.Parse(body, boundary);
-
-    var imageData = parts[0].data
-
-    var convertedResult = Buffer.from(imageData).toString('base64');
+    var parts = multipart.Parse(body, boundary)
+    var image = parts[0].data
+    
+    var result = await analyzeImage(image)
 
     context.res = {
-        body: convertedResult
+        body: { result }  
     };
+}
+
+async function analyzeImage(img){
+    
+    const subscriptionKey = process.env.FACEAPI_KEY1;
+    const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect';
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'
+        })
+        
+        let resp = await fetch(uriBase + '?' + params.toString(), {
+            method: 'POST',  
+            body: img,
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                'Ocp-Apim-Subscription-Key': subscriptionKey
+            }
+        })
+    
+        let data = await resp.json();
+        return data;
 }
