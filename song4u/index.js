7a484aaf0b4b998879f6ae5ed3187704fdc87619
mjsigg <mjsigg@gmail.com>
2021-07-15 20:00:08 -0500

    Sent an image over twilio and received results of age

--- song4u/index.js
+++ song4u/index.js
@@ -1,16 +1,64 @@
 const querystring = require('querystring');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     var reqbody = req.body
     context.log(reqbody)
 
     const queryObject = querystring.parse(req.body);
-
     var url = queryObject.MediaUrl0
 
+    let resp = await fetch(url,{
+        method: 'GET',
+    })
+
+    let data = await resp.arrayBuffer()
+    let age_data = await analyzeImage(data)
+    let age = age_data[0].faceAttributes.age
+
+    let generation = determine_generation(age)
+
+    context.log(generation)
+
     context.res = {
-        body: url
-     };
-     
+        body: generation
+    };   
 }
 
+function determine_generation(age){
+    let generation;
+    if(age > 5 && age < 25) {
+        generation = 'GenZ';
+    } else if (age > 24 && age < 41) {
+        generation = "GenY";
+    } else if (age > 40 && age < 57) {
+        generation = "GenX";    
+    } else if (age > 56 && age < 76) {
+        generation = "BabyBoomers";
+    } else  {
+        generation = "Unknown";
+    }
+    return generation;
+}
+
+async function analyzeImage(img){
+    
+    const subscriptionKey = process.env.FACEAPI_KEY1;
+    const uriBase = process.env.FACEAPI_ENDPOINT + '/face/v1.0/detect';
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'age'
+        })
+
+        let resp = await fetch(uriBase + '?' + params.toString(), {
+            method: 'POST',  
+            body: img,
+            headers: {
+                'Content-Type': 'application/octet-stream',
+                'Ocp-Apim-Subscription-Key': subscriptionKey
+            }
+        })
+    
+        let result = await resp.json();
+        return result;
+}
