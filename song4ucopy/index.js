b988a4e43930c0aa2b203989c29ae4d41e4d8cc5
mjsigg <mjsigg@gmail.com>
2021-07-15 20:32:40 -0500

    Updated secret

---
+++ song4ucopy/index.js
@@ -0,0 +1,15 @@
+const querystring = require('querystring');
+
+module.exports = async function (context, req) {
+    var reqbody = req.body
+    context.log(reqbody)
+
+    const queryObject = querystring.parse(req.body);
+
+    var url = queryObject.MediaUrl0
+
+    context.res = {
+        body: url
+     };
+
+}
