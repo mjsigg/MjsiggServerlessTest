368722625e6436bd3b05ed0c11d4ac81d339f0f7
mjsigg <mjsigg@gmail.com>
2021-07-15 23:51:27 -0500

    Tried reinstalling query string

--- song4u/index.js
+++ song4u/index.js
@@ -18,30 +18,30 @@ module.exports = async function (context, req) {
 
     let generation = determine_generation(age)
     
-    function determine_generation(age){
-        let generation;
-        if(age > 5 && age < 25) {
+function determine_generation(age){
+    let generation;
+
+    if(age > 5 && age < 25) {
             generation = 'GenZ';
-        } else if (age > 24 && age < 41) {
+    } else if (age > 24 && age < 41) {
             generation = "GenY";
-        } else if (age > 40 && age < 57) {
+    } else if (age > 40 && age < 57) {
             generation = "GenX";    
-        } else if (age > 56 && age < 76) {
+    } else if (age > 56 && age < 76) {
             generation = "BabyBoomers";
-        } else  {
+    } else  {
             generation = "Unknown";
-        }
-        return generation;
     }
-    context.log(generation)
+    return generation;
+ }    
 
     const songs = {
     
-    "GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
-    "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
-    "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
-    "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
-    "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+        "GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+        "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+        "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+        "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+        "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
 
     let song_classification = songs[generation]
 
