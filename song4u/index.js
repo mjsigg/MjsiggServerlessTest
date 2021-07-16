088e53898e0257da87d65021ad59af4158bcc9ff
mjsigg <mjsigg@gmail.com>
2021-07-15 22:36:42 -0500

    Completed the task and tested it.

--- song4u/index.js
+++ song4u/index.js
@@ -20,8 +20,22 @@ module.exports = async function (context, req) {
 
     context.log(generation)
 
+    const songs = {
+    
+    "GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+    "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+    "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+    "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+    "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
+    let song_classification = songs[generation]
+
+    let songs_output = `We guessed you're part of this generation: ${generation} Happy listening! ${song_classification}`
+
+    context.log(songs_output)
+
     context.res = {
-        body: generation
+        body: songs_output
     };   
 }
 
