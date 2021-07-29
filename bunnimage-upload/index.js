b3b7818cdf8d12be1b5048a6987ec51b2959b0de
mjsigg <mjsigg@gmail.com>
2021-07-29 16:03:53 -0500

    Finished the step

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -6,35 +6,44 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var boundary = multipart.getBoundary(req.headers['content-type']);
-    var body = req.body;
-    var parsedBody = multipart.Parse(body, boundary);
+    var the_header_value = req.headers['codename'];
 
-    var filetype = parsedBody[0].type;
-    if (filetype == "image/png") {
-        ext = "png";
-    } else if (filetype == "image/jpeg") {
-        ext = "jpeg";
-    } else if (filetype == "image/jpg") {
-        ext = "jpg"
+    var responseMessage = ""
+    if (body == null) {
+        responseMessage = "Sorry! No image attached."
     } else {
-        username = "invalidimage"
-        ext = "";
+        var password = the_header_value
+        var boundary = multipart.getBoundary(req.headers['content-type']);
+        var body = req.body;
+        var parsedBody = multipart.Parse(body, boundary);
+        
+        var filetype = parsedBody[0].type;
+        if (filetype == "image/png") {
+            ext = "png";
+        } else if (filetype == "image/jpeg") {
+            ext = "jpeg";
+        } else if (filetype == "image/jpg") {
+            ext = "jpg"
+        } else {
+            username = "invalidimage"
+            ext = "";
+        }
+        responseMessage = await uploadFile(parsedBody, ext, password);
     }
-    var responseMessage = await uploadFile(parsedBody, ext);
 
     context.res = {
         body: responseMessage
     };
 
-}
+}      
+
 async function uploadFile(parsedBody, ext) {
     
     const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
     const containerName = "1bitcamp";
     const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container  
     
-    const blobName = 'test.' + ext;    // Create the container
+    const blobName = password + "." + ext;    // Create the container
     const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
 
     const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
